a177cb2e350e14eb2be0d52b73ed0d5581cdad66
Hanson <hanson.hua903@gmail.com>
2023-05-27 01:36:40 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(){
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({
+            name: 'Steve',
+            email: 'steve@steve.com'
+        })
+    })
+    .then(resp=>resp.json())
+    .then(data=>{
+        const newId = data.id
+        const newIdElement = document.createElement('p')
+        newIdElement.setAttribute('id',`${newId}`)
+        document.body.appendChild(newIdElement)
+    })
+    .catch(error=>{
+        const errorMessage = 'Unauthorized Access'
+        const errorElement = document.createElement('p')
+        errorElement.textContent = errorMessage
+        document.body.appendChild(errorElement)
+    })
+}
